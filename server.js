727031eded9f4707bb99e3b440435e27408f9f0f
mohamedInfath <mohamedinfath99@gmail.com>
2023-02-18 16:01:24 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import colors from 'colors';
+import morgan from 'morgan';
+import dotenv from 'dotenv';
+
+
+// dotenv config
+dotenv.config();
+
+
+const app = express();
+
+
+// middleware
+app.use(express.json());
+app.use(morgan('dev'));
+
+
+// routes
+app.get('/', (req, res)  => {
+    res.status(200).send({
+        message : "server is running"
+    })
+});
+
+//listen port
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log(`Server is running ${process.env.NODE_MODE} mode on port ${process.env.PORT}`.bgYellow.black);
+})
