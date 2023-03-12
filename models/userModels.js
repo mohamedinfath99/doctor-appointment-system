a84247fe280a652c4e0d1c6efc605092f9fda4bf
mohamedInfath <mohamedinfath99@gmail.com>
2023-03-12 23:59:52 +0530

    model schema added

--- models/userModels.js
+++ models/userModels.js
@@ -4,15 +4,15 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: [true, 'name is require']
+        required: [true, 'name is required']
     },
     email: {
         type: String,
-        required: [true, 'email is require']
+        required: [true, 'email is required']
     },
     password: {
         type: String,
-        required: [true, 'password is require']
+        required: [true, 'password is required']
     }
 })
 
