74b7e9eb9f3294dcf3f33152b828343de3e66a1a
mohamedInfath <mohamedinfath99@gmail.com>
2023-03-13 12:10:17 +0530

    initial state change false

--- client/src/redux/features/alertSlice.js
+++ client/src/redux/features/alertSlice.js
@@ -4,7 +4,7 @@ import { createSlice } from '@reduxjs/toolkit';
 export const alertSlice = createSlice({
     name: "alerts",
     initialState: {
-        loading: false
+        loading: false,
     },
     reducers: {
         showLoading: (state) => { 
