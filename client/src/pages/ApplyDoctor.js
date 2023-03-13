c3238648154de987883a5c97c59fbf9a7f8d8586
mohamedInfath <mohamedinfath99@gmail.com>
2023-03-13 22:10:30 +0530

    Apply doctot created

---
+++ client/src/pages/ApplyDoctor.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/Layout'
+
+const ApplyDoctor = () => {
+  return (
+    <Layout>
+      <h1>apply Doctor</h1>
+    </Layout>
+  )
+}
+
+export default ApplyDoctor
