bf9a90d34f4e7f7f3f481845999e1200f9083445
mohamedInfath <mohamedinfath99@gmail.com>
2023-03-13 10:25:39 +0530

    authControllers added

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -7,15 +7,15 @@ import jwt from 'jsonwebtoken';
 export const registerControllers = async (req, res) => {
     try {
 
-        const exisitingUser = await User.findOne({email: req.body.email})
+        const exisitingUser = await User.findOne({ email: req.body.email })
 
-        if (exisitingUser){
-            return res.status(200).send({ message: 'User Already Exist', success: false})
+        if (exisitingUser) {
+            return res.status(200).send({ message: 'User Already Exist', success: false })
         }
 
         const password = req.body.password
         const salt = await bycrypt.genSalt(10)
-        const hashedPassword = await bycrypt.hash(password,salt)
+        const hashedPassword = await bycrypt.hash(password, salt)
         req.body.password = hashedPassword;
 
         const newUser = new User(req.body);
@@ -38,28 +38,60 @@ export const registerControllers = async (req, res) => {
 
 
 
-export const LoginControllers = async (req, res) => { 
+export const LoginControllers = async (req, res) => {
     try {
-        const user = await User.findOne({ email: req.body.email})
+        const user = await User.findOne({ email: req.body.email })
 
-        if(!user){
-            return res.status(200).send({ message: 'User not found', success: false})
+        if (!user) {
+            return res.status(200).send({ message: 'User not found', success: false })
         }
 
         const isMatch = await bycrypt.compare(req.body.password, user.password)
-        
-        if(!isMatch) {
+
+        if (!isMatch) {
             return res.status(200).send({ message: 'Invalid Email or Password', success: false })
         }
 
-        const token = jwt.sign({id: user._id},process.env.JWT_SECRET, {expiresIn: "1d"})
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1d" })
 
-        res.status(200).send({ message: 'Login Success', success: true, token})
-        
-    } 
+        res.status(200).send({ message: 'Login Success', success: true, token })
+
+    }
     catch (error) {
         console.log(error);
-        res.status(500).send({message: `Error in Login CTRL ${error.message}`});
-        
+        res.status(500).send({ message: `Error in Login CTRL ${error.message}` });
+
+    }
+}
+
+
+
+export const authControllers = async (req, res) => {
+    try {
+        const user = await User.findOne({ _id: req.body.userId})
+
+        if(!user){
+            return res.status(200).send({
+                message: 'User not found',
+                success: false
+            })
+        }
+        else {
+            res.status(200).send({
+                success: true,
+                data: {
+                    name: user.name,
+                    email: user.email,
+                }
+            })
+        }
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).send({
+            message: 'auth error',
+            success: false,
+            error
+        })
     }
 }
