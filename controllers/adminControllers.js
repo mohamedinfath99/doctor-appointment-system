1468f2bc047c3d8750d3406884c1e62b245d6b13
mohamedInfath <mohamedinfath99@gmail.com>
2023-03-15 00:22:16 +0530

    changeAccountStatusController implemented

--- controllers/adminControllers.js
+++ controllers/adminControllers.js
@@ -43,3 +43,38 @@ export const getAllDoctorController = async (req, res) => {
         })
     }
 }
+
+
+
+export const changeAccountStatusController = async (req, res) => {
+    try {
+
+        const { doctorId, status } = req.body;
+        const doctor = await Doctor.findByIdAndUpdate(doctorId, {status});
+        const user = await User.findOne({_id: doctor.userId}); 
+        const notification = user.notification;
+
+        notification.push({
+            type: 'doctor-account-request-updated',
+            message: `Your doctor account request has ${status}`,
+            onClickPath: '/notification'
+        })
+
+        user.isDoctor = status === "approved" ? true : false;
+        await user.save();
+        
+        res.status(201).send({
+            success: true,
+            message: 'Account status updated',
+            data: doctor
+        })
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            message: 'Error in account status',
+            error
+        })
+    }
+}
