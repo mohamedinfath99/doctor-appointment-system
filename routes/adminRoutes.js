699c4a7b9c7796d8847629954c9208ad109d8467
mohamedInfath <mohamedinfath99@gmail.com>
2023-03-15 00:21:05 +0530

    changeAccountStatus routes created

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,15 +1,23 @@
 import express from 'express';
-import { getAllDoctorController, getAllUsersController } from '../controllers/adminControllers.js';
+import { getAllDoctorController, getAllUsersController, changeAccountStatusController } from '../controllers/adminControllers.js';
 import authMiddlewares from '../middlewares/authMiddlewares.js';
 
 
 // Router object
 const router = express.Router()
 
+
+//Get users list
 router.get('/getAllUsers', authMiddlewares, getAllUsersController);
 
 
+//Get doctors list
 router.get('/getAllDoctors', authMiddlewares, getAllDoctorController)
+
+
+//Account status 
+router.post('/changeAccountStatus', authMiddlewares, changeAccountStatusController)
+
 
 
 export default router;
