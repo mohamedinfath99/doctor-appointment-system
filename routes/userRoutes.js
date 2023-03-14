c20f33d622fe7328086c418bbd9ca2bfa361902b
mohamedInfath <mohamedinfath99@gmail.com>
2023-03-14 19:02:05 +0530

    newly created apply-doctor, get-all-notification, delete-all-notification path

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,22 +1,29 @@
 import express from 'express';
-import { LoginControllers, registerControllers, authControllers, applyDoctorController } from '../controllers/userControllers.js';
+import { LoginControllers, registerControllers, authControllers, applyDoctorController, getAllNotificationController, deleteAllNotificationController } from '../controllers/userControllers.js';
 import authMiddlewares from '../middlewares/authMiddlewares.js';
 
 
-// router object
+// Router object
 const router = express.Router()
 
 
-// routes
+// Routes
 router.post('/register', registerControllers)
 router.post('/login', LoginControllers)
 
 
-// Auth || Post
+// Auth 
 router.post('/getUserData', authMiddlewares, authControllers)
 
 
-// apply doctor || Post
+// Apply doctor
 router.post('/apply-doctor', authMiddlewares, applyDoctorController)
+
+// Notification doctor
+router.post('/get-all-notification', authMiddlewares, getAllNotificationController)
+
+
+// Notification doctor
+router.post('/delete-all-notification', authMiddlewares, deleteAllNotificationController)
 
 export default router;
