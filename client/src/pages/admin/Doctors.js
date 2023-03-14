eba4ad0506609589a209f2610be0be0aaa9e9a55
mohamedInfath <mohamedinfath99@gmail.com>
2023-03-15 00:27:05 +0530

    handleAccountStatus part created

--- client/src/pages/admin/Doctors.js
+++ client/src/pages/admin/Doctors.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Layout from '../../components/Layout';
 import axios from 'axios';
-import { Table } from 'antd';
+import { message, Table } from 'antd';
 
 const Doctors = () => {
 
@@ -24,8 +24,33 @@ const Doctors = () => {
 
 
     }
-  }
+  };
 
+
+
+  // handle Account
+  const handleAccountStatus = async (record, status) => {
+    try {
+      const res = await axios.post('/api/v1/admin/changeAccountStatus', 
+      {doctorId: record._id, userId: record.userId, status: status}, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem('token')}`
+        }
+      })
+
+      if(res.data.success){
+        message.success(res.data.message);
+        window.location.reload();
+      }
+    } 
+    catch (error) {
+      message.error('Something went to wrong');
+      
+    }
+  };
+  
+  
+  
   useEffect(() => {
     getDoctors()
   }, []);
@@ -54,7 +79,7 @@ const Doctors = () => {
       dataIndex: 'actions',
       render: (text, record) => (
         <div className='d-flex'>
-          {record.status === 'pending' ? <button className='btn btn-success'>Approve</button> : <button className='btn btn-danger'>Reject</button>}
+          {record.status === 'pending' ? <button className='btn btn-success' onClick={() => handleAccountStatus(record, 'approved')}>Approve</button> : <button className='btn btn-danger'>Reject</button>}
         </div>
       )
     }
